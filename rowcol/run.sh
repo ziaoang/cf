fd6e6c1c63daa79afe6730fb6eacef0b9947a9db
ziaoang <ziaoang@gmail.com>
2016-04-26 00:28:07 +0800

    check y

--- rowcol/run.sh
+++ rowcol/run.sh
@@ -1,2 +1,4 @@
-../tool/liblinear-2.1/train -s 11 train.rc train.model
-../tool/liblinear-2.1/predict test.rc train.model test.out
+../tool/liblinear-2.1/train -s 11 train.rc 1.model
+../tool/liblinear-2.1/predict test.rc 1.model 1.out
+../tool/liblinear-2.1/train -s 11 train.rc2 2.model
+../tool/liblinear-2.1/predict test.rc2 2.model 2.out
