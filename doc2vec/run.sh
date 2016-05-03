2db4ff51ae47645454be47d056b824efda9ade3d
ziaoang <ziaoang@gmail.com>
2016-05-04 01:39:13 +0800

    some change

--- doc2vec/run.sh
+++ doc2vec/run.sh
@@ -1,4 +1,4 @@
-python vector.py ../data/my/train-1m.txt data/model-1m.txt
-python vector.py ../data/my/train-10m.txt data/model-10m.txt
-python vector.py ../data/my/train-20m.txt data/model-20m.txt
+python feature.py ../data/my/train-1m.txt data/model-1m.txt
+python feature.py ../data/my/train-10m.txt data/model-10m.txt
+python feature.py ../data/my/train-20m.txt data/model-20m.txt
 
